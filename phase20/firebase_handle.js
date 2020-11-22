64fa179ef7fdbd56bdb5cd22e43629da041f034a
Grant Weiss <grantweiss@Grants-MacBook-Pro.local>
2020-11-21 23:46:04 -0500

    updates

---
+++ phase20/firebase_handle.js
@@ -0,0 +1,13 @@
+
+  var firebaseConfig = {
+    apiKey: "AIzaSyDTIlxZikBc1K07YKo8phfxYT-xDpvCMb0",
+    authDomain: "myptp-f79fb.firebaseapp.com",
+    databaseURL: "https://myptp-f79fb.firebaseio.com",
+    projectId: "myptp-f79fb",
+    storageBucket: "myptp-f79fb.appspot.com",
+    messagingSenderId: "509346938413",
+    appId: "1:509346938413:web:810e82cf96bc344d1702a4"
+  };
+  firebase.initializeApp(firebaseConfig);
+
+  
